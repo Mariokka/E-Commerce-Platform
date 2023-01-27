dcec260f72fa53eb49f419eb7c4016cd1e5d1930
mokatova <mokatova@student.42wolfsburg.de>
2023-01-27 14:51:14 +0300

    Set up the project

---
+++ src/main/java/com/mummys_knitting/demo/controllers/MainController.java
@@ -0,0 +1,12 @@
+package com.mummys_knitting.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+    @GetMapping
+    public String root() {return "Hello World!";}
+}
